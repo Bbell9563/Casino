997e5e74b5e2eca41da445317a96f5990e39eb17
Jeffrey Soriano <jeffsor17@gmail.com>
2020-02-14 15:44:59 -0700

    added starting menu

--- app.rb
+++ app.rb
@@ -1,3 +1,33 @@
 require_relative = 'deck'
 require_relative = 'dice'
 
+class App 
+    def initialize
+        menu
+    end
+    def menu 
+        puts '++++++++++++++++++++++'
+        puts 'Welcome to ruby casino'
+        puts '++++++++++++++++++++++'
+        puts 'Please verify your age:'
+        @user_age = gets.to_i
+        user_entry()
+        #welcome user??
+        menu_games()
+
+    end
+    def user_entry
+        if @user_age >= 21
+            menu_games()
+        else
+            
+            exit
+        end
+    end
+    def menu_games
+
+    end
+
+
+end
+App.new
