532fc44c5a6b1463aa4e1778a8a93234f30983ed
Jeffrey Soriano <jeffsor17@gmail.com>
2020-02-14 17:04:29 -0700

    added app menu linked to user class

--- app.rb
+++ app.rb
@@ -1,39 +1,69 @@
-require_relative = 'deck'
-require_relative = 'dice'
-require_relative = 'user'
+# require_relative = 'deck'
+# require_relative = 'dice'
+require_relative 'user'
 
 class App 
     def initialize
-        menu
+        @user = User.new
+        menu()
     end
-
-    def create_user
-
     def menu 
         puts '++++++++++++++++++++++'
         puts 'Welcome to ruby casino'
         puts '++++++++++++++++++++++'
         puts 'Please verify your age:'
-        @user_age = gets.to_i
+        @user_age = @user.user_age
         user_entry()
-        #welcome user??
         menu_games()
-
     end
     def user_entry
         if @user_age >= 21
-          
-          menu_games()
+            user_greet()
         else
-          puts "Come back when you are 21"
-            
+            puts 'you\'re too young sorry, have a nice day :)'
             exit
         end
     end
+    def user_greet
+        puts 'What is your name?'
+        @user_name = @user.user_name.chomp
+        puts '++++++++++++++++++++++'
+        puts "Hello #{@user_name} please choose a game"
+        puts '++++++++++++++++++++++'
+    end
     def menu_games
+        puts '1) Slots'
+        puts '2) High or Low'
+        puts '3) Blackjack'
+        puts '4) War'
+        puts '5) exit'
+        puts '++++++++++++++++++++++'
+        user_choice = gets.to_i
+        case user_choice
+        when 1 
+            puts 'you chose Slots'
+        when 2
+            puts 'you chose High or Low'
+        when 3
+            puts 'you chose Blackjack'
+        when 4
+            puts 'you chose War'
+        when 5 
+            puts '++++++++++++++++++++++'
+            puts 'OK, have a nice day :)'
+            puts '++++++++++++++++++++++'
+            exit
+        else
+            puts '++++++++++++++++++++++'
+            puts 'Invalid choice, please try again'
+            puts '++++++++++++++++++++++'
+            menu_games()
+        end
 
     end
 
 
 end
-App.new
+
+word = App.new
+puts word
--- user.rb
+++ user.rb
@@ -1,10 +1,15 @@
-require_relative 'wallet'
+
 
 class User
-  attr_accessor :name, :age
-  def initialize(name, age)
-    @name = name
-    @age = age
-  end
-
+    attr_accessor 
+    def initialize
+        @user_name 
+        @user_age 
+    end
+    def user_name
+        @user_name = gets.to_s
+    end
+    def user_age
+        @user_age = gets.to_i
+    end
 end
