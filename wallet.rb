64a8b6275ad17788d3e60c7bbc9235895bb51bdb
Brianna Bell <bbell9563@gmail.com>
2020-02-14 17:07:00 -0700

    wallet

--- wallet.rb
+++ wallet.rb
@@ -1,6 +1,19 @@
 class Wallet
-  def initialize
+  attr_accessor :amount
+  def initialize(amount)
+    @amount = amount
+  end
 
+  def add(number)
+    @amount += number
+
+  end
+  def remove(number)
+    @amount -= number
   end
 
 end
+
+wallet = Wallet.new(100)
+wallet.add(20)
+puts wallet.amount
